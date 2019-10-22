dfd1f2505bc9bdd10f8aa0c9fc1a6e87f2e830bc
akum55 <amit.kumar5@nike.com>
2019-10-22 16:57:27 -0700

    Countries REST API Version 1.0

--- src/main/java/com/uni/countries/Application.java
+++ src/main/java/com/uni/countries/Application.java
@@ -2,26 +2,14 @@ package com.uni.countries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.ApplicationContext;
 
-import java.util.Arrays;
-
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
 
+
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-
-        Arrays.sort(beanNames);
-
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+        SpringApplication.run(Application.class, args);
     }
-
 }
 
 
--- src/main/java/com/uni/countries/resource/CountryListResource.java
+++ src/main/java/com/uni/countries/resource/CountryListResource.java
@@ -1,8 +1,28 @@
 package com.uni.countries.resource;
 
+import com.uni.countries.model.Country;
+import com.uni.countries.process.CountryListProcess;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CountryListResource {
 
+    CountryListProcess countryListProcess = new CountryListProcess();
+
+
+    @RequestMapping(value="/countries", method= RequestMethod.GET, produces="application/json")
+    public List<Country> getCountries() {
+        return countryListProcess.getAllCountry();
+    }
+
+    @RequestMapping(value="/countries/{countryId}", method= RequestMethod.GET, produces="application/json")
+    public Country getCountry(@PathVariable("countryId") Long countryId) {
+        return countryListProcess.getCountry(countryId);
+    }
 
 }
-
-
